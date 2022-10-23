19f702d31bc3cfe7f4dd48202b9bffe1931f8252
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2022-10-23 16:22:20 +0700

    fix implicit copy assigment

--- mesh/src/hollow_box.cpp
+++ mesh/src/hollow_box.cpp
@@ -4,7 +4,7 @@
 
 hollow_box::hollow_box(int n, float dx, float dy, float dz, float in_dx,
                        float in_dz)
-    : mesh(16 + (n - 1) * 8, 12 + (n - 1) * 6) {
+    : mesh(16 + (n - 1) * 8, 16 + (n - 1) * 6) {
   float hdx = dx / 2.0, hdy = dy / 2.0, hdz = dz / 2.0, in_hdz = in_dz / 2,
         r = in_dx / 2, angle = M_PI / n;
 
@@ -54,8 +54,9 @@ hollow_box::hollow_box(int n, float dx, float dy, float dz, float in_dx,
   this->quads.push_back(
       mesh::quad({4 * n + 4, 5 * n + 4, 7 * n + 6, 6 * n + 6}, cid++));
 
-  quad_strip = {0, 1, n + 2, n + 1, 3 * n + 3, 3 * n + 4, 2 * n + 3, 2 * n + 2};
-  for (auto p = quad_strip.begin(); p < quad_strip.end() - 3; p += 2) {
+  auto small_strip = {0,         1,         n + 2,     n + 1,
+                      3 * n + 3, 3 * n + 4, 2 * n + 3, 2 * n + 2};
+  for (auto p = small_strip.begin(); p < small_strip.end() - 3; p += 2) {
     for (int i = 0; i < n; i++) {
       this->quads.push_back(
           mesh::quad({p[0] + i, p[1] + i, p[2] + i, p[3] + i}, cid++));
