76eb28780a9c0c92b5c37b8bfb6c54b717fc4e18
Duy Khang <khangemail22@gmail.com>
2022-11-29 13:43:27 +0700

    switch to perspective camera

--- scene/src/camera.cpp
+++ scene/src/camera.cpp
@@ -15,26 +15,30 @@ void Scene::Camera::view() {
              int(this->screen_width / this->aspect_ratio));
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  if (this->dimension == Scene::Camera::Dimension::THREE &&
-      this->distance != 0) {
-    auto eyeX = this->distance * std::sin(angle * DEG_TO_RAD);
-    auto eyeY = this->height;
-    auto eyeZ = this->distance * std::cos(angle * DEG_TO_RAD);
-    gluLookAt(eyeX, eyeY, eyeZ, look_at.x, look_at.y, look_at.z, 0, 1, 0);
+  if (this->dimension == Scene::Camera::Dimension::THREE) {
+    auto eye = Point3(float(this->distance * std::sin(angle * DEG_TO_RAD)),
+                      float(this->height),
+                      float(this->distance * std::cos(angle * DEG_TO_RAD)));
+    auto up = this->distance != 0
+                  ? Vector3(0, 1, 0)
+                  : Vector3(float(-std::sin(angle * DEG_TO_RAD)), 0,
+                            float(-std::cos(angle * DEG_TO_RAD)));
+    gluLookAt(eye.x, eye.y, eye.z, look_at.x, look_at.y, look_at.z, up.dx,
+              up.dy, up.dz);
   } else {
-    gluLookAt(0, look_at.y + 1, 0, look_at.x, look_at.y, look_at.z,
-              -std::sin(angle * DEG_TO_RAD), 0, -std::cos(angle * DEG_TO_RAD));
+    gluLookAt(0, 1000, 0, 0, 0, 0, 0, 0, 1);
   }
-
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-
-  auto factor = max(this->distance, 1);
-
-  if (this->aspect_ratio <= 1.0)
-    glOrtho(-factor, factor, -factor / this->aspect_ratio,
-            factor / this->aspect_ratio, -1000.0, 1000.0);
-  else
-    glOrtho(-factor * this->aspect_ratio, factor * this->aspect_ratio, -factor,
-            factor, -1000.0, 1000.0);
+  if (this->dimension == Scene::Camera::Dimension::THREE) {
+    gluPerspective(60.0, this->aspect_ratio, 1.0, 1000.0);
+  } else {
+    auto factor = 1.0f;
+    if (this->aspect_ratio <= 1.0)
+      glOrtho(-factor, factor, -factor / this->aspect_ratio,
+              factor / this->aspect_ratio, -1000, 1000);
+    else
+      glOrtho(-factor * this->aspect_ratio, factor * this->aspect_ratio,
+              -factor, factor, -1000, 1000);
+  }
 }
