7beeb6cd00a24642e167d5b008853bc3c06564e4
Duy Khang <khangemail22@gmail.com>
2022-12-01 17:06:36 +0700

    clean up axis

--- scene/src/scene.cpp
+++ scene/src/scene.cpp
@@ -210,20 +210,6 @@ void Scene::display() {
 
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glColor3f(0, 0, 1);
-  glBegin(GL_LINES);
-  glVertex3f(0, 0, 0);
-  glVertex3f(4, 0, 0);
-
-  glVertex3f(0, 0, 0);
-  glVertex3f(0, 4, 0);
-
-  glVertex3f(0, 0, 0);
-  glVertex3f(0, 0, 4);
-  glEnd();
-
-  glColor3f(0, 0, 0);
-
   for (auto &obj : this->objs) {
     obj.draw(this->t);
   }
